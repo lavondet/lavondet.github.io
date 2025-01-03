be73bbed056258e1dcc21f14b2895c5ba2ea2bf5
LouisAvondet <avondet2005@gmail.com>
2025-01-03 16:19:51 -0500

    Implemented navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import React from "react";
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+
+      <Navbar />
 
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+
+function Navbar() {
+    return(
+      <div class="navbar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#journey">Journey</a>
+        <a href="#skills">Skills</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+    );
+}
+
+export default Navbar;
