672fa5136ad923cd7d9138fe76169ea5d10c2386
LouisAvondet <avondet2005@gmail.com>
2025-01-04 23:00:15 -0500

    Navbar change

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Navbar() {
     return(
       <div class="navbar">
-        <a href="#home">Home</a>
+        <a id="home-link" href="#home">Home</a>
         <a href="#about">About</a>
         <a href="#journey">Journey</a>
         <a href="#skills">Skills</a>
