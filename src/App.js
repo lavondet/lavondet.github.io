ed4a3c0133e06408afff9ddcfd783a26b33443e2
Your Name <avondet2005@gmail.com>
2025-01-18 19:05:36 -0500

    rearrange + new divider

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
-import './App.css';
+import "./App.css";
+import "./components/Navbar.css";
+import "./components/Welcome.css";
+import "./components/About.css";
+import "./components/Journey.css";
+import "./components/Skills.css";
+import "./components/Projects.css";
+import "./components/Footer.css";
 import React from "react";
-import Navbar from './Navbar';
-import Welcome from './Welcome';
-import About from './About';
-import Journey from './Journey';
-import Skills from './Skills';
-import Projects from './Projects';
-import Footer from './Footer';
+import Navbar from "./components/Navbar";
+import Welcome from "./components/Welcome";
+import About from "./components/About";
+import Journey from "./components/Journey";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-
       <Navbar />
 
-      <Welcome />
-
       <div class="subsections">
+        <Welcome />
 
-        <About />
+        <div className="wave">
+          <div className="wave-divider">
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                class="shape-fill"
+              ></path>
+            </svg>
+          </div>
+
+          <About />
+        </div>
 
         <Journey />
 
         <Skills />
 
         <Projects />
-
       </div>
 
       <Footer />
-
     </div>
   );
 }
